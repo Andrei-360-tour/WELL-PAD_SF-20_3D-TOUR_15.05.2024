c16ac230baab1676b1d70932403cde4d148ae16f
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-06-10 15:50:01 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,447 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sf-20_overview",
+      "name": "SF-20_Overview",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.750703462613533,
+        "pitch": 0.6829582456635208,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2091279169288622,
+          "pitch": 0.47933850426554336,
+          "rotation": 0,
+          "target": "1-sf-20_wellhead--pipeline"
+        },
+        {
+          "yaw": -1.9148426708749309,
+          "pitch": 0.532986012346143,
+          "rotation": 0,
+          "target": "6-sf-20_concrete-pad"
+        },
+        {
+          "yaw": -2.4654513752979295,
+          "pitch": 0.30193436796326445,
+          "rotation": 0,
+          "target": "4-sf-20_well-pad--pipeline"
+        },
+        {
+          "yaw": -1.9300254040455194,
+          "pitch": 0.33622313595786224,
+          "rotation": 0,
+          "target": "5-sf-20_well-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sf-20_wellhead--pipeline",
+      "name": "SF-20_Wellhead & pipeline",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4275047012303936,
+        "pitch": 0.7551335858124268,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9783254883896575,
+          "pitch": -0.0658808581286916,
+          "rotation": 0,
+          "target": "0-sf-20_overview"
+        },
+        {
+          "yaw": 0.3156439306391192,
+          "pitch": 0.9752703583299223,
+          "rotation": 0,
+          "target": "2-sf-20_wellhead-"
+        },
+        {
+          "yaw": 1.9158766649758432,
+          "pitch": 0.3799061866701887,
+          "rotation": 0,
+          "target": "6-sf-20_concrete-pad"
+        },
+        {
+          "yaw": 0.43439723799936836,
+          "pitch": 0.4002129027087058,
+          "rotation": 0,
+          "target": "3-sf-20_pipeline"
+        },
+        {
+          "yaw": -0.41882895871268744,
+          "pitch": 0.07957774147831742,
+          "rotation": 0,
+          "target": "4-sf-20_well-pad--pipeline"
+        },
+        {
+          "yaw": 0.9182196550732336,
+          "pitch": 0.10293529699687021,
+          "rotation": 0,
+          "target": "5-sf-20_well-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sf-20_wellhead-",
+      "name": "SF-20_Wellhead ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.38188922027705097,
+        "pitch": 0.5799677233667069,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03394343413570766,
+          "pitch": 0.23208736889491277,
+          "rotation": 0,
+          "target": "1-sf-20_wellhead--pipeline"
+        },
+        {
+          "yaw": -1.2655492091225806,
+          "pitch": 0.17166736979268293,
+          "rotation": 0,
+          "target": "3-sf-20_pipeline"
+        },
+        {
+          "yaw": -0.7076461819492366,
+          "pitch": 0.000035820948887987925,
+          "rotation": 0,
+          "target": "5-sf-20_well-pad"
+        },
+        {
+          "yaw": 0.5222897173841634,
+          "pitch": 0.19734385702239265,
+          "rotation": 0,
+          "target": "6-sf-20_concrete-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sf-20_pipeline",
+      "name": "SF-20_Pipeline",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.12139248231295241,
+        "pitch": 0.6436368347143606,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.916373496498359,
+          "pitch": 0.03873376131716988,
+          "rotation": 0,
+          "target": "4-sf-20_well-pad--pipeline"
+        },
+        {
+          "yaw": 0.9941563421900952,
+          "pitch": 0.3265692572488934,
+          "rotation": 0,
+          "target": "2-sf-20_wellhead-"
+        },
+        {
+          "yaw": -0.09088912479013089,
+          "pitch": 0.1592907025461212,
+          "rotation": 0,
+          "target": "6-sf-20_concrete-pad"
+        },
+        {
+          "yaw": 1.1385876239826267,
+          "pitch": -0.2922903947308324,
+          "rotation": 0,
+          "target": "1-sf-20_wellhead--pipeline"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sf-20_well-pad--pipeline",
+      "name": "SF-20_Well Pad & pipeline",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.120497172184808,
+          "pitch": 0.02591527001706595,
+          "rotation": 0,
+          "target": "3-sf-20_pipeline"
+        },
+        {
+          "yaw": 1.3469676596194127,
+          "pitch": 0.07677697700285435,
+          "rotation": 0,
+          "target": "5-sf-20_well-pad"
+        },
+        {
+          "yaw": 1.836306778212367,
+          "pitch": 0.309011725840886,
+          "rotation": 0,
+          "target": "1-sf-20_wellhead--pipeline"
+        },
+        {
+          "yaw": 2.649007980471117,
+          "pitch": -0.1273862533624719,
+          "rotation": 0,
+          "target": "0-sf-20_overview"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sf-20_well-pad",
+      "name": "SF-20_Well Pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9317371274097246,
+          "pitch": 0.13751843731945002,
+          "rotation": 0,
+          "target": "4-sf-20_well-pad--pipeline"
+        },
+        {
+          "yaw": 1.4403813348745107,
+          "pitch": 0.06265356354921003,
+          "rotation": 0,
+          "target": "6-sf-20_concrete-pad"
+        },
+        {
+          "yaw": 1.793168813453783,
+          "pitch": 0.06869249044463466,
+          "rotation": 0,
+          "target": "2-sf-20_wellhead-"
+        },
+        {
+          "yaw": 1.6954328023094583,
+          "pitch": -0.04155835895391746,
+          "rotation": 0,
+          "target": "1-sf-20_wellhead--pipeline"
+        },
+        {
+          "yaw": 1.668570078549683,
+          "pitch": -0.1696423212616338,
+          "rotation": 0,
+          "target": "0-sf-20_overview"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sf-20_concrete-pad",
+      "name": "SF-20_Concrete Pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.824360327696505,
+        "pitch": 0.36233512457841144,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5308077552470074,
+          "pitch": 0.1107405732395712,
+          "rotation": 0,
+          "target": "5-sf-20_well-pad"
+        },
+        {
+          "yaw": 0.4311821350192453,
+          "pitch": 0.2281384011672074,
+          "rotation": 0,
+          "target": "2-sf-20_wellhead-"
+        },
+        {
+          "yaw": -0.9359348634285585,
+          "pitch": -0.30574103681331977,
+          "rotation": 0,
+          "target": "0-sf-20_overview"
+        },
+        {
+          "yaw": 1.2908180193357133,
+          "pitch": 0.2995052046968425,
+          "rotation": 0,
+          "target": "3-sf-20_pipeline"
+        },
+        {
+          "yaw": 0.1555646497670402,
+          "pitch": -0.26578803596231815,
+          "rotation": 0,
+          "target": "1-sf-20_wellhead--pipeline"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WELL PAD SF-20",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
